81c1f63d41287e06f074a7fe0e8959d06b5de480
YTMini <minitech@YTMinis-Mac-mini.local>
2025-05-26 22:06:30 -0700

    updated models to handle arcgis fires

--- project_wildfire/project_wildfire_web/Models/DTO/FireDTO.cs
+++ project_wildfire/project_wildfire_web/Models/DTO/FireDTO.cs
@@ -9,26 +9,31 @@ namespace project_wildfire_web.Models.DTO
 {
 public partial class FireDTO
 {
+    [Required]
+    [Name("FireId")]
     public string FireId { get; set; } 
 
-        [Required]
-        [Name("latitude")]
-        public decimal Latitude { get; set; }
+    [Required]
+    [Name("Latitude")]
+    public decimal Latitude { get; set; }
 
-        [Required]
-        [Name("longitude")]
-        public decimal Longitude { get; set; }
+    [Required]
+    [Name("Longitude")]
+    public decimal Longitude { get; set; }
 
-        //[Required]
-        //public Geometry Location { get; set; } = null!;
+    //[Required]
+    //public Geometry Location { get; set; } = null!;
+    [Required]
+    public DateTime StartDate { get; set; }
 
 
-        [Required]
-        [Name("frp")]
-        public decimal RadiativePower { get; set; }
+
+    [Required]
+    [Name("RadiativePower")]
+    public decimal? RadiativePower { get; set; }
 
         //For admin created fires
-         public bool IsAdminFire { get; set; } 
+    public bool IsAdminFire { get; set; } = false;
 
 }
 
@@ -63,10 +68,12 @@ namespace project_wildfire_web.ExtensionsMethods
         {
             return new Fire
             {
+                FireId = fireDTO.FireId,
                 Latitude = fireDTO.Latitude,
                 Longitude = fireDTO.Longitude,
                 RadiativePower = fireDTO.RadiativePower,
-                IsAdminFire = fireDTO.IsAdminFire
+                IsAdminFire = fireDTO.IsAdminFire,
+                StartDate = fireDTO.StartDate
             };
         }
     }
--- project_wildfire/project_wildfire_web/Models/Fire.cs
+++ project_wildfire/project_wildfire_web/Models/Fire.cs
@@ -4,19 +4,19 @@ namespace project_wildfire_web.Models;
 
 public partial class Fire
 {
-    public string FireId { get; set; }
+    public string? FireId { get; set; }
     public string Name { get; set; }
     public decimal Latitude { get; set; }
     public decimal Longitude { get; set; }
-    public decimal AcreageBurned { get; set; }
-    public decimal PercentageContained { get; set; }
-    public string POOCounty { get; set; }
-    public string POOState { get; set; }
-    public string FireCause { get; set; }
-    public DateTime StartDate { get; set; }
+    public decimal? AcreageBurned { get; set; }
+    public decimal? PercentageContained { get; set; }
+    public string? POOCounty { get; set; }
+    public string? POOState { get; set; }
+    public string? FireCause { get; set; }
+    public DateTime? StartDate { get; set; }
     public decimal? RadiativePower { get; set; }
 
-    public bool IsAdminFire { get; set; }
+    public bool IsAdminFire { get; set; } = false;
 
     //public Geometry? Polygon { get; set; }
 
